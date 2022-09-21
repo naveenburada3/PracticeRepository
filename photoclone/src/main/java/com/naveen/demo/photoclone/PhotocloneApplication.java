e765ba710d3cab3d192ebb7693756e729fc8c964
unknown <naveen.burada@capgemini.com>
2022-09-21 20:38:57 +0530

    FirstCommit

---
+++ photoclone/src/main/java/com/naveen/demo/photoclone/PhotocloneApplication.java
@@ -0,0 +1,13 @@
+package com.naveen.demo.photoclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotocloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotocloneApplication.class, args);
+	}
+
+}
---
+++ photoclone/src/test/java/com/naveen/demo/photoclone/PhotocloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naveen.demo.photoclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotocloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
